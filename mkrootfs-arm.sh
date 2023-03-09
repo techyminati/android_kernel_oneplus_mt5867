a949b9820d6008b0fbf2957410f94c3d6b14a383
techyminati <sinha.aryan03@gmail.com>
2023-03-09 06:17:44 +0100

    kernel-4.19: Drop all weird scripts from mstar
    * Not needed on AOSP build env at all
    
    Change-Id: Ib24c6b99d986f135bdbe1484a6600cc600365052
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- mkrootfs-arm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd arch
-cd ramdisk
-tar jxvf rootfs_32_64_kernel.tar.bz2
-cd ../../
-chown -R $(whoami) arch/ramdisk/rootfs
-chmod -R 755 arch/ramdisk/rootfs
--- rmrootfs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-[ -e arch/ramdisk/rootfs ] && sudo chown $USER:users arch/ramdisk/rootfs -R && rm arch/ramdisk/rootfs -rf
